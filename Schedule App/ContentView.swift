ce6fc5628a2f64b30b23e56aac1518220604ada3
Leo dos Remedios <leondosr@outlook.com>
2024-06-04 17:38:21 -0500

    Started Basic Layout for Schedule (Day of week, date, buttons)

--- Schedule App/ContentView.swift
+++ Schedule App/ContentView.swift
@@ -9,50 +9,55 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    
 
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
+    var body: some View { //basic layout for day selection
+        VStack{
+            Text("Stinky Phooc App")
+                .font(.title)
+                .bold()
+            
+            Rectangle()
+                .frame(height: 1)
+                .foregroundColor(.blue)
+            
+            HStack{ //horizontally stack buttons and date
+                //back button
+                Button {
+                    print("forward")
+                } label:{
+                    Image(systemName: "lessthan")
+                        .bold()
                 }
-                .onDelete(perform: deleteItems)
+                
+                Spacer()
+                    .frame(width: 30)
+                
+                //Day of week and Date
+                VStack{
+                    Text("Tuesday")
+                        .font(.title2)
+                        .bold()
+                    Text("June 4, 2024")
+                }
+                
+                Spacer()
+                    .frame(width: 30)
+                
+                //forward button
+                Button {
+                    print("forward")
+                } label:{
+                    Image(systemName: "greaterthan")
+                        .bold()
+                }
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+            
         }
+        .padding()
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+   
 }
 
 #Preview {
