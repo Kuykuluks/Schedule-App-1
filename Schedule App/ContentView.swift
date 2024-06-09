eef3686b0614bdd6554867f877039c6884da6022
Leo dos Remedios <leondosr@outlook.com>
2024-06-09 16:27:03 -0500

    Added Layout to select Time Frame

--- Schedule App/ContentView.swift
+++ Schedule App/ContentView.swift
@@ -9,50 +9,94 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    
 
     var body: some View { //basic layout for day selection
+        //title
         VStack{
-            Text("Stinky Phooc App")
+            Text("Scheduling App")
                 .font(.title)
                 .bold()
             
+                
             Rectangle()
                 .frame(height: 1)
                 .foregroundColor(.blue)
-            
-            HStack{ //horizontally stack buttons and date
-                //back button
-                Button {
-                    print("forward")
-                } label:{
-                    Image(systemName: "lessthan")
-                        .bold()
+                
+            //time selection
+            VStack{
+                Text("Select a Time Frame")
+                    .font(.title2)
+                    .bold()
+                
+                HStack{ //horizontally stack buttons and date
+                    //back button
+                    Button {
+                        print("forward")
+                    } label:{
+                        Image(systemName: "lessthan")
+                            .bold()
+                    }
+                    
+                    Spacer()
+                        .frame(width: 30)
+                    
+                    //Day of week and Date
+                    VStack{
+                        Text("Tuesday")
+                            .font(.title3)
+                        Text("June 4, 2024")
+                    }
+                    
+                    Spacer()
+                        .frame(width: 30)
+                    
+                    //forward button
+                    Button {
+                        print("forward")
+                    } label:{
+                        Image(systemName: "greaterthan")
+                            .bold()
+                    }
                 }
-                
-                Spacer()
-                    .frame(width: 30)
-                
-                //Day of week and Date
-                VStack{
-                    Text("Tuesday")
-                        .font(.title2)
-                        .bold()
-                    Text("June 4, 2024")
-                }
-                
-                Spacer()
-                    .frame(width: 30)
-                
-                //forward button
-                Button {
-                    print("forward")
-                } label:{
-                    Image(systemName: "greaterthan")
-                        .bold()
+                VStack(spacing: 20) {
+                    Button{
+                        
+                    }label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .frame(height: 100)
+                            Text("Opening Shift")
+                                .bold()
+                                .font(.title2)
+                                .foregroundColor(.white)
+                        }
+                    }
+                    Button{
+                        
+                    }label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .frame(height: 100)
+                            Text("Day Shift")
+                                .bold()
+                                .font(.title2)
+                                .foregroundColor(.white)
+                        }
+                    }
+                    Button{
+                        
+                    }label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 25)
+                                .frame(height: 100)
+                            Text("Closing Shift")
+                                .bold()
+                                .font(.title2)
+                                .foregroundColor(.white)
+                        }
+                    }
                 }
             }
-            
         }
         .padding()
     }
