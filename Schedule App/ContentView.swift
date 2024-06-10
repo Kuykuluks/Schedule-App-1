e9ccea248c08164c0cac57783cc4f30125f3794d
Leo dos Remedios <leondosr@outlook.com>
2024-06-10 16:01:52 -0500

    Added Navigation To another screen

--- Schedule App/ContentView.swift
+++ Schedule App/ContentView.swift
@@ -9,106 +9,108 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
+    @State var selectedMonth = 0
 
     var body: some View { //basic layout for day selection
         //title
-        VStack{
-            Text("Stinky Phooc")
-                .font(.title)
-                .bold()
-            
-                
-            Rectangle()
-                .frame(height: 1)
-                .foregroundColor(.blue)
-                
-            //time selection
+        NavigationStack {
             VStack{
-                Text("Select a Time Frame")
-                    .font(.title2)
+                Text("Stinky Phooc")
+                    .font(.title)
                     .bold()
                 
-                HStack{ //horizontally stack buttons and date
-                    //back button
-                    Button {
-                        print("forward")
-                    } label:{
-                        Image(systemName: "lessthan")
-                            .bold()
-                    }
                     
-                    Spacer()
-                        .frame(width: 30)
+                Rectangle()
+                    .frame(height: 1)
+                    .foregroundColor(.blue)
                     
-                    //Day of week and Date
-                    VStack{
-                        Text("Tuesday")
-                            .font(.title3)
-                        Text("June 4, 2024")
-                    }
+                //time selection
+                VStack{
+                    Text("Select a Time Frame")
+                        .font(.title2)
+                        .bold()
                     
-                    Spacer()
-                        .frame(width: 30)
-                    
-                    //forward button
-                    Button {
-                        print("forward")
-                    } label:{
-                        Image(systemName: "greaterthan")
-                            .bold()
-                    }
-                }
-                VStack(spacing: 20) {
-                    Button{
-                        
-                    }label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 25)
-                                .strokeBorder(.blue, lineWidth: 3)
-                                .fill(.clear)
-                                .frame(height: 100)
-                            Text("Opening Shift")
+                    HStack{ //horizontally stack buttons and date
+                        //back button
+                        Button {
+                            print("forward")
+                        } label:{
+                            Image(systemName: "lessthan")
                                 .bold()
-                                .font(.title2)
-                                .foregroundColor(.blue)
+                        }
+                        
+                        Spacer()
+                            .frame(width: 30)
+                        
+                        //Day of week and Date
+                        VStack{
+                            Text("Tuesday")
+                                .font(.title3)
+                            Text("June 4, 2024")
+                        }
+                        
+                        Spacer()
+                            .frame(width: 30)
+                        
+                        //forward button
+                        Button {
+                            print("forward")
+                        } label:{
+                            Image(systemName: "greaterthan")
+                                .bold()
                         }
                     }
-                    Button{
-                        
-                    }label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 25)
-                                .strokeBorder(.blue, lineWidth: 3)
-                                .fill(.clear)
-                                .frame(height: 100)
-                            Text("Day Shift")
-                                .bold()
-                                .font(.title2)
-                                .foregroundColor(.blue)
+                    VStack(spacing: 20) {
+                        NavigationLink{
+                            EmptyView()
+                        }label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 25)
+                                    .strokeBorder(.blue, lineWidth: 3)
+                                    .fill(.clear)
+                                    .frame(height: 100)
+                                Text("Opening Shift")
+                                    .bold()
+                                    .font(.title2)
+                                    .foregroundColor(.blue)
+                            }
                         }
-                    }
-                    Button{
-                        
-                    }label: {
-                        ZStack{
-                            RoundedRectangle(cornerRadius: 25)
-                                .strokeBorder(.blue, lineWidth: 3)
-                                .fill(.clear)
-                                .frame(height: 100)
-                            Text("Closing Shift")
-                                .bold()
-                                .font(.title2)
-                                .foregroundColor(.blue)
+                        NavigationLink{
+                            EmptyView()
+                        }label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 25)
+                                    .strokeBorder(.blue, lineWidth: 3)
+                                    .fill(.clear)
+                                    .frame(height: 100)
+                                Text("Day Shift")
+                                    .bold()
+                                    .font(.title2)
+                                    .foregroundColor(.blue)
+                            }
+                        }
+                        NavigationLink{
+                            EmptyView()
+                        }label: {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 25)
+                                    .strokeBorder(.blue, lineWidth: 3)
+                                    .fill(.clear)
+                                    .frame(height: 100)
+                                Text("Closing Shift")
+                                    .bold()
+                                    .font(.title2)
+                                    .foregroundColor(.blue)
+                            }
                         }
                     }
                 }
             }
+            .padding()
         }
-        .padding()
     }
-
-   
 }
+
 
 #Preview {
     ContentView()
